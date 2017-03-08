35fe3fefdf0fd1fcb53be0751b7b4133da86ceed
Betty Baker <betty@heysparkbox.com>
2017-03-08 15:43:23 -0500

    :racehorse: feat: Add sizes class

---
+++ size-class_caffeinated-beverage.rb
@@ -0,0 +1,25 @@
+class Size
+  def short
+    short = "short (8oz)"
+  end
+
+  def tall
+    tall = "tall (12oz)"
+  end
+
+  def grande
+    grande = "grande (16oz)"
+  end
+
+  def venti
+    venti = "venti (20oz)"
+  end
+
+  def venti_iced
+    venti_iced = "venti iced (24oz)"
+  end
+
+  def trenta
+    trenta = "trenta (31oz)"
+  end
+end
