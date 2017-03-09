c278c51f8b2d01f162213803f077c7ad101aa55e
Betty Baker <betty@heysparkbox.com>
2017-03-09 11:31:06 -0500

    :racehorse: feat: Remove puts from all methods

--- size-class_caffeinated-beverage.rb
+++ size-class_caffeinated-beverage.rb
@@ -1,25 +1,25 @@
 class Size
   def short
-    puts "short (8oz)"
+    "short (8oz)"
   end
 
   def tall
-    puts "tall (12oz)"
+    "tall (12oz)"
   end
 
   def grande
-    puts "grande (16oz)"
+    "grande (16oz)"
   end
 
   def venti
-    puts "venti (20oz)"
+    "venti (20oz)"
   end
 
   def venti_iced
-    puts "venti iced (24oz)"
+    "venti iced (24oz)"
   end
 
   def trenta
-    puts "trenta (31oz)"
+    "trenta (31oz)"
   end
 end
