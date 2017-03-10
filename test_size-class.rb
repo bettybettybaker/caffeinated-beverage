e049da4f0d04f08032865bf2e2228370a69066af
Betty Baker <betty@heysparkbox.com>
2017-03-10 11:13:21 -0500

    :racehorse: test: Test all sizes in the class Size

--- test_size-class.rb
+++ test_size-class.rb
@@ -6,4 +6,25 @@ class TestSize < Minitest::Test
   def test_short
     assert_equal "8oz", Size.new.size(:short)
   end
+
+  def test_tall
+    assert_equal "12oz", Size.new.size(:tall)
+  end
+
+  def test_grande
+    assert_equal "16oz", Size.new.size(:grande)
+  end
+
+  def test_venti
+    assert_equal "20oz", Size.new.size(:venti)
+  end
+
+  def test_venti_iced
+    assert_equal "24oz", Size.new.size(:venti_iced)
+  end
+
+  def test_trents
+    assert_equal "31oz", Size.new.size(:trenta)
+  end
+
 end
