8807319444d6f557e474960a0bb068e240668e73
Betty Baker <betty@heysparkbox.com>
2017-03-10 09:55:11 -0500

    :racehorse: feat: Create hash for sizes

--- size-class_caffeinated-beverage.rb
+++ size-class_caffeinated-beverage.rb
@@ -1,25 +1,10 @@
 class Size
-  def short
-    "short (8oz)"
+  def size(size_name)
+    "#{possible_sizes[size_name]}oz"
   end
-
-  def tall
-    "tall (12oz)"
-  end
-
-  def grande
-    "grande (16oz)"
-  end
-
-  def venti
-    "venti (20oz)"
-  end
-
-  def venti_iced
-    "venti iced (24oz)"
-  end
-
-  def trenta
-    "trenta (31oz)"
+  def possible_sizes
+    {
+      short: 8
+    }
   end
 end
