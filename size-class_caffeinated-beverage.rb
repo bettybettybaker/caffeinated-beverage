cfc4433f267f1d44f657aee4076c32b013ad82b6
Betty Baker <betty@heysparkbox.com>
2017-03-10 11:13:56 -0500

    :racehorse: feat: Add all sizes to hash

--- size-class_caffeinated-beverage.rb
+++ size-class_caffeinated-beverage.rb
@@ -4,7 +4,12 @@ class Size
   end
   def possible_sizes
     {
-      short: 8
+      short: 8,
+      tall: 12,
+      grande: 16,
+      venti: 20,
+      venti_iced: 24,
+      trenta: 31
     }
   end
 end
